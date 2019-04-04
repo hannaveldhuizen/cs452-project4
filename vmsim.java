efce0dbd7505ad5ee1080a17dbe7b9f2225e9e2e
hannaveldhuizen <hannaveldhuizen@Hannas-MacBook-Pro.local>
2019-04-03 19:38:46 -0700

    first commit

---
+++ vmsim.java
@@ -0,0 +1,48 @@
+/*
+ * Author: Hanna Veldhuizen
+ * CSC 452 -- Project 4
+ * Virtual Memory Simulator
+ * Due: April 8, 2019
+ *
+ * Command for running this code:
+ *    vmsim -n <numframes> -a <opt|clock|fifo|rand> <tracefile>
+ */
+
+
+public class vmsim {
+    public static int main(String[] args) {
+        return 0;
+    }
+
+    /*
+     * opt() -- method for simulating what the optimal page replacement algorithm would
+     * choose if it had perfect knowledge
+     */
+    public static int opt() {
+        return 0;
+    }
+
+    /*
+     * clock() -- method for executing the clock algorithm. The clock algorithm returns the
+     * same as the second-chance algorithm, but it just has a different method of getting
+     * there.
+     */
+    public static int clock() {
+        return 0;
+    }
+
+    /*
+     * fifo() -- method for "First In-First Out" replacement algorithm. Kicks out the oldest
+     * page in memory.
+     */
+    public static int fifo() {
+        return 0;
+    }
+
+    /*
+     * random() -- picks a page at random.
+     */
+    public static int random() {
+        return 0;
+    }
+}
